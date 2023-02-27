5b8ee85cf57e4232f210ba81ac77c3d92c1a14e8
Friedonuts32 <98046392+Josh-Fried@users.noreply.github.com>
2023-02-27 10:53:56 -0700

    fixed

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-idef add(a, b):
+def add(a, b):
     return a + b
 
 
@@ -8,7 +8,7 @@ def test_add():
 
 
 def subtract(a, b):
-    return a + b  # <--- fix this in step 8
+    return a - b  # <--- fix this in step 8
 
 
 # uncomment the following test in step 5
